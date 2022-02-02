50cf3f48047cea5257b3f13181245b466d73c93f
vcokltfre <vcokltfre@gmail.com>
2022-02-02 15:09:41 +0000

    chore: switch to adapter-static

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from "@sveltejs/adapter-cloudflare";
+import adapter from "@sveltejs/adapter-static";
 import preprocess from "svelte-preprocess";
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -12,7 +12,10 @@ const config = {
   ],
 
   kit: {
-    adapter: adapter(),
+    adapter: adapter({
+      pages: "build",
+      assets: "build",
+    }),
     target: "#svelte",
     vite: {
       css: {
